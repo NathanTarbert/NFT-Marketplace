2566c36b8ac508debc061608863f6a4f35e528d9
Nathan Tarbert <nate4t@hotmail.comm>
2021-12-17 20:13:35 -0800

    created a function to fetch NFTs created and sold along with a function to fetch the data from all market items

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -77,5 +77,83 @@ contract NFTMarket is ReentrancyGuard {
             price,
             false
         )
-    };
+    }
+
+    function createMarketSale(
+        address nftContract,
+        uint256 itemId
+    )   public payable nonReentrant {
+    uint price = idToMarkItem[itemId].price;
+    uint tokenId = idToMarkItem[itemId].tokenId;
+    require(msg.value == price, 'Please submit the asking price in order to complete the purchase');
+
+    idToMarkItem[itemId].seller.transfer(msg.value);
+    IERC721(nftContract).transferFrom(address(this), msg.sender, tokenId);//this will transfer the ownership from the contract address(owner)
+    idToMarkItem[itemId].owner = payable(msg.sender);
+    _itemsSold.increment();//update the number by 1
+    payable(owner).transfer(listingPrice);//pay the owner of the contract / commision
+    }
+
+    function fetchMarketItems() public view returns (MarketItem[] memory) { //returns an array of market items to view on the client side
+        uint itemCount = _itemIds.current();
+        uint unsoldItemCount = _itemIds.current() - _itemsSold.current();
+        uint currentIndex = 0;
+
+        MarketItem[] memory items = new MarketItem[](unsoldItemCount); //create a new array to loop over
+        for (uint i = 0; i < itemCount; i++) {
+            if (idToMarketItem[i + 1].owner == address(0)) {
+                uint currentId = idToMarketItem[i + 1].itemId;
+                MarketItem storage currentItem = idToMarketItem(currentId);
+                    items[currentIndex] = currentItem;
+                    currentIndex += 1;
+            }
+        }
+        return items;
+    }
+
+    function fetchMyNFTs() public view returns (MarketItem[] memory) { //fetch the nfts that have been created by the contract owner
+        uint totalItemCount = _itemIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+
+        for (uint i = 0; i < totalItemCount; i++) {
+            if (idToMarketItem[i + 1].owner == msg.sender) {
+                itemCount += 1;
+            }
+        }
+
+        MarketItem[] memory items = new MarketItem[](itemCount);
+            for (uint i = 0; i < totalItemCount; i++) {
+                if (idToMarketItem[i + 1].owner == msg.sender) {
+                    uint currentId = idToMarketItem[i + 1].itemSold;
+                    MarketItem storage currentItem = idToMarketItem[currentId];
+                    items[currentIndex] = currentItem;
+                    currentIndex += 1;
+                }
+            }
+            return items;
+    }
+
+    function fetchItemsCreated() public view returns (MarketItems[] memory) {//fetching the items created by the seller
+        uint totalItemCount = _itemIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+
+        for (uint i = 0; i < totalItemCount; i++) {//loop over items and return the item count
+            if (idToMarketItem[i + 1].seller == msg.sender) {//if found we update the value by incrementing by 1
+                itemCount += 1;
+            }
+        }
+
+        MarketItem[] memory items = new MarketItem[](itemCount);
+         for (uint i = 0; i < totalItemCount; i++) {
+            if (idToMarketItem[i + 1].seller == msg.sender) {//check to see if the seller address is equal the msg.sender
+                uint currentId = idToMarketItem[i + 1].itemSold;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+    return items;
+    }
 }
