333be26dd3772423810f9985f583bd86924c2b23
Nathan Tarbert <nate4t@hotmail.comm>
2021-12-20 22:16:41 -0800

    updated config.js, next.config.js and fixed bugs in create-item page

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,24 @@
 require("@nomiclabs/hardhat-waffle");
-require("dotenv").config();
-require("solidity-coverage");
+// require("@nomiclabs/hardhat-ganache");
+// require("dotenv").config();
+// require("solidity-coverage");
 
 const fs = require('fs');
 const privateKey = fs.readFileSync('.secret').toString();//this will reference our metamask private key
 
+const projectId = '4625d8a5e77f4c678b39423652c6f6cd';//this id should be brought in from the .env file for security
+
 module.exports = {
    networks: {
     hardhat: {
-      chainId: 1337
+      chainId: 31337
     },
     mumbai: {
-      url: `https://polygon-mumbai.infura.io/v3/${process.env.PROJECT-ID}`,
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
       accounts: [privateKey]
     },
     mainnet: {
-      url: `https://polygon-mainnet.infura.io/v3/${process.env.PROJECT-ID}`,
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
       accounts: [privateKey]  
     }
   },
